0d006deb58614beabf37624c6df395afaa8bc070
Geoffrey <you@example.comcohengmc@gmail.com>
2024-05-18 09:11:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A little playground where I can mess around while learning some basic react stuff :)
+18 May 2024
---
+++ index.js
@@ -0,0 +1,13 @@
+let rootEl = document.getElementById("root");
+
+let nav = (
+<nav>
+    <h1>Home Page</h1>
+    <ul>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+
+</nav>);
+
+ReactDOM.render(nav,rootEl);
